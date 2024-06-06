6271f3316f71432c3d426ce464fc28ce60494737
Youri Schijff <90404399+YouriSchijff@users.noreply.github.com>
2024-06-06 17:32:47 +0200

    Update Dependencies.h

--- Lila/src/Platform/Dependencies.h
+++ Lila/src/Platform/Dependencies.h
@@ -2,29 +2,12 @@
 
 #include "Lila/Core/Logger.h"
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
 static bool initDependencies() {
-	if(!glfwInit()) {
-		lila_fatal("Failed to initalize GLFW!")
-		return false;
-	}
-
-	lila_trace("Initialized GLFW");
-	lila_info("GLFW version %d.%d.%d", GLFW_VERSION_MAJOR, GLFW_VERSION_MINOR, GLFW_VERSION_REVISION);
 
 	return true;
 }
 
 static bool initOpenGL() {
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		lila_fatal("Failed to initalize OpenGL!")
-		return false;
-	}
-
-	lila_trace("Initialized OpenGL");
-	lila_info("OpenGL version %s", glGetString(GL_VERSION));
 
 	return true;
 }
